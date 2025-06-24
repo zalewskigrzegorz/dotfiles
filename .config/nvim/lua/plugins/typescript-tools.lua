39b4062873b78d542f1808bc709d38d0e68c38b2
Grzegorz Zalewski <grzegorz.zalewski@redocly.com>
2025-06-24 20:05:45 +0200

    Added the nvim config file and the gitleaks pre-commit hook 🔧

---
+++ .config/nvim/lua/plugins/typescript-tools.lua
@@ -0,0 +1,31 @@
+return {
+  -- Disable vtsls
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = { enabled = false },
+        tsserver = { enabled = false },
+      },
+    },
+  },
+
+  -- Add typescript-tools.nvim
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+    },
+  },
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Dotfiles
+This is my dtfiles repository feel free to use it or fork it.
+
+## Install with stow
+
+```bash
+stow .
+```
