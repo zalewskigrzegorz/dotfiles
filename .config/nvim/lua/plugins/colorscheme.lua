b713160f6292617e681e9e334d41a18e5d3bc512
Grzegorz Zalewski <grzegorz.zalewski@redocly.com>
2025-07-02 23:12:06 +0200

    change nvim dracula repo to fix accesebility issues

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
   -- add dracula
-  { "Mofiqul/dracula.nvim" },
+  { "binhtran432k/dracula.nvim" },
 
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "dracula-soft",
     },
   },
 }
