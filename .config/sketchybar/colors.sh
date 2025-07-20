ae01255f263799c17b7962cbc4977d2cb68a940d
Grzegorz Zalewski <grzegorz.zalewski@redocly.com>
2025-07-20 02:04:34 +0200

    update nvim config add sketchybar

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+### Dracula Pro Color Theme for SketchyBar
+
+# Dracula Pro base colors
+export BLACK=0xff22212c      # Background
+export WHITE=0xfff8f8f2      # Foreground
+export RED=0xffff9580        # AnsiColor1
+export GREEN=0xff8aff80      # AnsiColor2
+export BLUE=0xff9580ff       # AnsiColor4
+export YELLOW=0xffffff80     # AnsiColor3
+export ORANGE=0xffffca80     # Orange
+export MAGENTA=0xffff80bf    # AnsiColor5
+export CYAN=0xff80ffea       # AnsiColor6
+export PURPLE=0xffaa99ff     # AnsiColor12 (bright blue)
+export GREY=0xff7970a9       # Comment
+export LIGHT_GREY=0xffc6c6c2 # Dim foreground
+export DARK_GREY=0xff454158  # Selection
+export TRANSPARENT=0x00000000
+
+# Specialized Dracula battery colors
+export BATTERY_1=0xff8aff80 # Green - Full
+export BATTERY_2=0xffffff80 # Yellow - High
+export BATTERY_3=0xffffca80 # Orange - Medium
+export BATTERY_4=0xffff9580 # Red - Low
+export BATTERY_5=0xffe67373 # Functional Red - Critical
+
+# General bar colors
+export BAR_COLOR=$TRANSPARENT            # Transparent bar
+export BAR_BORDER_COLOR=0xff504c67       # Bright black border
+export BACKGROUND_1=0xff22212c           # Dracula background
+export BACKGROUND_2=0xff454158           # Selection background
+export ICON_COLOR=$WHITE                 # Foreground white icons
+export LABEL_COLOR=$WHITE                # Foreground white labels
+export POPUP_BACKGROUND_COLOR=0xff22212c # Dracula background
+export POPUP_BORDER_COLOR=0xff9580ff     # Dracula blue border
+export SHADOW_COLOR=$BLACK               # Background shadows
+
+# Dracula accent colors
+export ACCENT_PRIMARY=0xff9580ff    # Dracula Blue
+export ACCENT_SECONDARY=0xff8aff80  # Dracula Green
+export ACCENT_TERTIARY=0xffffca80   # Dracula Orange
+export ACCENT_QUATERNARY=0xffff80bf # Dracula Magenta
+export ACCENT_PINK=0xffff99cc       # Bright Magenta
