c2c8764a8581d2283b30f124d7a06c0178006313
Grzegorz Zalewski <grzegorz.zalewski@redocly.com>
2026-05-22 12:10:35 +0200

    fix(sketchybar): hardcode CONFIG_DIR path so event_providers spawn correctly 🩹

--- dot_config/sketchybar/items/widgets/cpu.lua
+++ dot_config/sketchybar/items/widgets/cpu.lua
@@ -4,7 +4,7 @@ local settings = require("settings")
 
 -- Execute the event provider binary which provides the event "cpu_update" for
 -- the cpu load data, which is fired every 2.0 seconds.
-sbar.exec("killall cpu_load >/dev/null; $CONFIG_DIR/helpers/event_providers/cpu_load/bin/cpu_load cpu_update 2.0")
+sbar.exec("killall cpu_load >/dev/null; /Users/greg/.config/sketchybar/helpers/event_providers/cpu_load/bin/cpu_load cpu_update 2.0")
 
 local cpu = sbar.add("graph", "widgets.cpu", 42, {
     position = "right",
